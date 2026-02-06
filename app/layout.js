47473749c4ca8846b2b3b311f1dccd96f05c5098
Pranjul <pranjul.here@gmail.com>
2026-02-06 10:07:05 +0530

    updatred the font theme to videotype

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,4 @@
-import { Press_Start_2P } from "next/font/google";
 import "./globals.css";
-
-const pressStart2P = Press_Start_2P({
-  weight: "400",
-  subsets: ["latin"],
-  variable: "--font-press-start",
-});
 
 export const metadata = {
   title: "CICtards | Insert Coin",
@@ -15,7 +8,7 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="dark">
-      <body className={`${pressStart2P.variable} font-mono antialiased bg-black text-white selection:bg-green-500 selection:text-black`}>
+      <body className={`antialiased bg-black text-white selection:bg-green-500 selection:text-black`}>
         {children}
       </body>
     </html>
